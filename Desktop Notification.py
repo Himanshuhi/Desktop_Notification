7c3a471b802a158d5cc54a5ea4905040f7aab6d9
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-12 19:18:16 +0530

    Add files via upload

---
+++ Desktop Notification.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+from plyer import notification
+
+title = input("Enter the title of the notification: ")
+message = input("Enter the message body of the notification: ")
+duration = int(input("Enter the duration of the notification (in seconds): "))
+
+# Get the time at which the notification should be displayed
+hours = int(input("Enter the hour at which the notification should be displayed (0-23): "))
+minutes = int(input("Enter the minute at which the notification should be displayed (0-59): "))
+notify_time = datetime.now().replace(hour=hours, minute=minutes)
+
+# Wait until the notification time is reached
+while datetime.now() < notify_time:
+    pass
+
+notification.notify(
+    title=title,
+    message=message,
+    app_name='Python Notification Demo',
+    timeout=duration
+)
+
+
+
+
+
+
+#pip install plyer
